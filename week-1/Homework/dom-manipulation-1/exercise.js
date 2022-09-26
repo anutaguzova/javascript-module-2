eb3335d48d59d4084f1b5c3c6660ce669157ae3f
Hanna Huzava <guzovaann@gmail.com>
2022-09-26 23:46:59 +0200

    feat: add HW - week 1 -dom manipulation 1

--- week-1/Homework/dom-manipulation-1/exercise.js
+++ week-1/Homework/dom-manipulation-1/exercise.js
@@ -10,11 +10,17 @@ Write JavaScript below that logs:
 
     3. the element with id "jumbotron-text"
     --> should log the "#jumbotron-text" node
+    
 
     4. all the "p" elements of contained inside  the .primary-content element node
     --> should log a list of nodes with a length of 3
 
 */
+
+console.log(document.querySelectorAll("p"))
+console.log(document.querySelector(".site-header"))
+console.log(document.querySelector("#jumbotron-text"))
+console.log(document.querySelectorAll(".primary-content p"))
 
 
 /*
@@ -24,6 +30,10 @@ Task 2
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
 
+document.querySelector("#alertBtn").addEventListener("click", () =>{
+ alert("Thanks for visiting Bikes for Refugees!")
+})
+
 
 /*
 Task 3
@@ -31,6 +41,9 @@ Task 3
 
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
+// document.querySelector("#bgrChangeBtn").addEventListener("click", () => {
+//     document.body.style.backgroundColor = "green"
+// })
 
 
 /*
@@ -38,7 +51,20 @@ Task 4
 ======
 
 When a user clicks the ‘Add some text’ button, a new paragraph should be added inside the section that says “LEARN MORE”
-*/
+// */
+document.querySelector("#addTextBtn").addEventListener("click", () => {
+    let article = document.createElement("article");
+    article.classList.add("article");
+
+    let text = document.createElement("p");
+    text.classList.add("article-lead");
+    text.innerText = "Many refugees";
+    text.style.color= "red"
+    article.appendChild(text);
+
+     document.querySelector("#mainArticles").appendChild(article)
+     
+})
 
 
 
@@ -48,6 +74,10 @@ Task 5
 
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
+document.querySelector("#largerLinksBtn").addEventListener("click", () => {
+    document.querySelector(".facebook-link").style.fontSize= "xx-large";
+    document.querySelectorAll(".nav-link").forEach((link)  =>{link.style.fontSize= "xx-large" })   
+})
 
 
 /*
@@ -59,6 +89,21 @@ When the 'Add' button is clicked, get the text inside the input field and create
 Also clear the text inside the input field
 */
 
+document.querySelector("#addArticleBtn").addEventListener("click", () => {
+    let article = document.createElement("article");
+    article.classList.add("article");
+
+    let text = document.createElement("p");
+    text.classList.add("article-lead");
+    text.style.color= "green";
+    text.innerHTML =document.querySelector(".addArticle").value;
+    
+    article.appendChild(text);
+    document.querySelector("#mainArticles").appendChild(article);
+
+     document.querySelector(".addArticle").value= "";
+})
+
 /*
 Task 7
 ======
@@ -67,4 +112,13 @@ Create an array of 5 different colors.
 Using the same function in Task 3, every time the 'Change colour' button is clicked, the background color will be changed with the next color in the array.
 The next color when you are in the last color of the array will be the first color again.
 */
+
+document.querySelector("#bgrChangeBtn").addEventListener("click", changecolor)
+
+function changecolor() {
+    let color = ["green", "red", "grey", "white", "brown"]
+    let i = color.indexOf(document.body.style.backgroundColor)
+    document.body.style.backgroundColor = color[(i+1) % color.length]
+
+}
 
